cc6f28b134428c3a7141cc6b047ec220df3fd9b4
Jose E <joseandroiddeveloper@gmail.com>
2018-12-03 21:36:18 -0500

    implemented Parcelable to my Movie Model class so that it can be saved in the onSavedInstance and be restored to help out with User interaction. Also organized files into packages for more coding readability and organization.

---
+++ app/src/main/java/com/jge/topratedmovies/Models/UserReview.java
@@ -0,0 +1,29 @@
+package com.jge.topratedmovies.Models;
+
+import com.google.gson.annotations.SerializedName;
+
+public class UserReview {
+
+
+    @SerializedName("content")
+    private String content;
+
+    @SerializedName("author")
+    private String author;
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
